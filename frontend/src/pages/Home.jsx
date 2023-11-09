7225af250662d4043b23ea3fe7a2269ce99fad4c
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-11-09 15:12:39 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
     useEffect(() => {
         setLoading(true);
         axios
-            .get('http://localhost:5555/books')
+            .get('https://bookstore-db.onrender.com/books')
             .then((response) => {
                 setBooks(response.data.data);
                 setLoading(false);
