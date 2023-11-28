114ebea553c59e5a183bc24fc615bdff2d24e04d
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-11-28 23:36:36 +0530

    Update BookCard.tsx

--- frontend-next/components/BookCard.tsx
+++ frontend-next/components/BookCard.tsx
@@ -7,9 +7,9 @@ const BookCard = () => {
           <div className='md:px-[5rem] px-[2rem] pt-0 pb-8 flex flex-wrap flex-row items-center justify-center gap-6'>
 
             {/* First Card */}
-            <div className='bg-[#fff] flex flex-col items-start md:w-[350px] w-[90%] shadow-xl'>
+            <div className='bg-[#fff] flex flex-col items-start md:w-[320px] w-[90%] shadow-xl'>
               <div>
-                <img src="/store-1.jpg" alt="Home Image" className='' />
+                <img src="/book-1.png" alt="Home Image" className='' />
               </div>
               <div className='p-4 flex flex-col items-start justify-start'>
                 <div>THE SUBTLE ART OF NOT GIVING A FUCK</div>
@@ -18,9 +18,9 @@ const BookCard = () => {
             </div>
 
             {/* Second Card */}
-            <div className='bg-[#fff] flex flex-col items-start md:w-[350px] w-[90%] shadow-xl'>
+            <div className='bg-[#fff] flex flex-col items-start md:w-[320px] w-[90%] shadow-xl'>
               <div>
-                <img src="/store-2.jpg" alt="Home Image" className='' />
+                <img src="/book-2.png" alt="Home Image" className='' />
               </div>
               <div className='p-4 flex flex-col items-start justify-start'>
                 <div>THE SUBTLE ART OF NOT GIVING A FUCK</div>
@@ -29,9 +29,9 @@ const BookCard = () => {
             </div>
 
             {/* Third Card */}
-            <div className='bg-[#fff] flex flex-col items-start md:w-[350px] w-[90%] shadow-xl'>
+            <div className='bg-[#fff] flex flex-col items-start md:w-[320px] w-[90%] shadow-xl'>
               <div>
-                <img src="/store-3.jpg" alt="Home Image" className='' />
+                <img src="/book-3.png" alt="Home Image" className='' />
               </div>
               <div className='p-4 flex flex-col items-start justify-start'>
                 <div>THE SUBTLE ART OF NOT GIVING A FUCK</div>
