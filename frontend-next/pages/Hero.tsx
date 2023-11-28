c5e5797e419cc063f1b505be1256a233f0d9606b
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-11-28 22:02:30 +0530

    Update Hero.tsx

--- frontend-next/pages/Hero.tsx
+++ frontend-next/pages/Hero.tsx
@@ -7,7 +7,7 @@ function Hero() {
     <div>
       <div id='home-head' className='' />
 
-      <div className='flex flex-col text-center w-full pt-[35px] pb-[40px]'>
+      <div className='flex flex-col text-center w-full pt-[35px] pb-[20px]'>
         <div className='lg:text-[25px] text-[20px] font-semibold mt-4 mb-4 text-black'>
             Trending <span className='text-[#22CFB8]'>Sets 🔥</span>
         </div>
@@ -18,12 +18,18 @@ function Hero() {
         <BookCard />
       </div>
 
-      <div className='flex flex-col w-full h-auto bg-[#ffffff]'>
-        <div className='lg:text-[25px] text-[20px] font-semibold mt-4 mb-4 text-black text-center'>
-            Why <span className='text-[#22CFB8]'>EBooksMania 🔥</span>
-        </div>
+      <div className='flex flex-col w-full items-center h-auto bg-[#ffffff]'>
+        <div className='flex flex-col text-center lg:w-full w-[90%] pt-[20px] pb-[40px] lg:px-[10%] px-4'>
+          <div className='lg:text-[25px] text-[20px] font-semibold mt-4 mb-4 text-black text-center'>
+              Why <span className='text-[#22CFB8]'>EBooksMania🚀</span>
+          </div>
+          <div className='mb-8 text-black'>
+              Presented herein are some of the most prevalent and sought-after sets in contemporary times.
+          </div>
 
-        <AboutCard />
+          <AboutCard />
+
+        </div> 
       </div>
 
     </div>
