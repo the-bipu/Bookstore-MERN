7ad28e0b9a912e50af426bcfbdc1adbee77e4bdf
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-11-29 13:02:41 +0530

    Update About.tsx

--- frontend-next/pages/About.tsx
+++ frontend-next/pages/About.tsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
+    <div>
+      <div className='heading-link'>
+        <div className='py-6'>
+          <h3>Our Books</h3>
+          <p> <a href="/">home</a> / books </p>
+        </div>
+      </div>
+      <div></div>
+    </div>
   )
 }
 
