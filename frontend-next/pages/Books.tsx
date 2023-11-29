3b7725a489729f43393b989ace2f5b72acc3245f
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-11-29 13:02:44 +0530

    Update Books.tsx

--- frontend-next/pages/Books.tsx
+++ frontend-next/pages/Books.tsx
@@ -1,8 +1,26 @@
+import TotalDepartment from '@/components/TotalDepartment'
 import React from 'react'
 
 function Books() {
   return (
-    <div>Books</div>
+    <div>
+      <div className='heading-link'>
+        <div className='py-6'>
+          <h3>Our Books</h3>
+          <p> <a href="/">home</a> / books </p>
+        </div>
+      </div>
+      <div className='py-8'>
+        <div className='lg:text-[25px] text-[20px] font-semibold mt-4 mb-4 text-black text-center'>
+              Our <span className='text-[#22CFB8]'>Departments 🍃</span>
+          </div>
+          <div className='mb-8 text-black text-center'>
+              Presented herein are some of the most prevalent and sought-after sets in contemporary times.
+          </div>
+
+        <TotalDepartment />
+      </div>
+    </div>
   )
 }
 
