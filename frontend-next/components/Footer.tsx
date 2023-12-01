46763cfaf565081210b904964fce8c58ff155771
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-12-01 20:48:40 +0530

    Update Footer.tsx

--- frontend-next/components/Footer.tsx
+++ frontend-next/components/Footer.tsx
@@ -7,7 +7,7 @@ import { FaGithub, FaLinkedinIn, FaDribbble, FaTwitter, FaBehance } from "react-
 const Footer = () => {
   
   return (
-    <div id='footer-main' className="md:w-full w-[90%] h-auto text-black md:px-[5rem] px-0 py-[2.5rem]">
+    <div id='footer-main' className="md:w-full w-[100%] h-auto text-black md:px-[5rem] px-0 py-[2.5rem]">
         <div className='flex md:flex-row flex-col items-start justify-around flex-wrap md:p-0 p-6 md:gap-0 gap-6'>
             <div className='md:w-[35%] w-auto flex flex-col gap-3'>
                 <div className='flex flex-row items-center gap-1 text-[22px]'>
@@ -27,30 +27,33 @@ const Footer = () => {
                 </div>
             </div>
             <div className='flex flex-col gap-2'>
-                <div className='text-[#bfbfbf] font-normal'>Quick Links</div>
+                <div className='text-[#bfbfbf] font-normal'>Our Info</div>
                 <div className='flex flex-col gap-1'>
-                    <a href='' className='font-normal text-[#000]'>items</a>
-                    <a href='' className='font-normal text-[#000]'>items</a>
-                    <a href='' className='font-normal text-[#000]'>items</a>
-                    <a href='' className='font-normal text-[#000]'>items</a>
+                    <a href='' className='font-normal text-[#000]'>College</a>
+                    <a href='' className='font-normal text-[#000]'>Departments</a>
+                    <a href='' className='font-normal text-[#000]'>Programmes</a>
+                    <a href='' className='font-normal text-[#000]'>E-Lib</a>
+                    <a href='/About' className='font-normal text-[#000]'>Website Team</a>
                 </div>
             </div>
             <div className='flex flex-col gap-2'>
                 <div className='text-[#bfbfbf] font-normal'>Quick Links</div>
                 <div className='flex flex-col gap-1'>
-                    <a href='' className='font-normal text-[#000]'>items</a>
-                    <a href='' className='font-normal text-[#000]'>items</a>
-                    <a href='' className='font-normal text-[#000]'>items</a>
-                    <a href='' className='font-normal text-[#000]'>items</a>
+                    <a href='' className='font-normal text-[#000]'>Home</a>
+                    <a href='' className='font-normal text-[#000]'>Books</a>
+                    <a href='' className='font-normal text-[#000]'>Subjects</a>
+                    <a href='' className='font-normal text-[#000]'>About</a>
+                    <a href='' className='font-normal text-[#000]'>Contact</a>
                 </div>
             </div>
             <div className='flex flex-col gap-2'>
-                <div className='text-[#bfbfbf] font-normal'>Quick Links</div>
+                <div className='text-[#bfbfbf] font-normal'>Extra Links</div>
                 <div className='flex flex-col gap-1'>
-                    <a href='' className='font-normal text-[#000]'>items</a>
-                    <a href='' className='font-normal text-[#000]'>items</a>
-                    <a href='' className='font-normal text-[#000]'>items</a>
-                    <a href='' className='font-normal text-[#000]'>items</a>
+                    <a href='' className='font-normal text-[#000]'>Sliet</a>
+                    <a href='' className='font-normal text-[#000]'>Sliet-LEET</a>
+                    <a href='' className='font-normal text-[#000]'>Ask questions</a>
+                    <a href='' className='font-normal text-[#000]'>Send Feedback</a>
+                    <a href='' className='font-normal text-[#000]'>Terms and conditions</a>
                 </div>
             </div>
         </div>
