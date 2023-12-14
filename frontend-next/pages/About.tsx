183e108699d6e1257eb7fae05b34b9f3f952a3f6
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-12-14 23:20:27 +0530

    Update About.tsx

--- frontend-next/pages/About.tsx
+++ frontend-next/pages/About.tsx
@@ -1,15 +1,28 @@
 import React from 'react'
+import AboutCard from '@/components/AboutCard'
 
 function About() {
   return (
-    <div>
+    <div className='gradientLinear'>
       <div className='heading-link'>
         <div className='py-6'>
           <h3>About Us</h3>
           <p> <a href="/">home</a> / about </p>
         </div>
       </div>
-      <div></div>
+      <div className='flex flex-col w-full items-center h-auto'>
+        <div className='flex flex-col text-center lg:w-full w-[90%] pt-[20px] pb-[40px] lg:px-[10%] px-4'>
+          <div className='lg:text-[25px] text-[20px] font-semibold mt-4 mb-4 text-black text-center'>
+            Why <span className='text-[#22CFB8]'>EBooksMania🚀</span>
+          </div>
+          <div className='mb-8 text-black'>
+            Presented herein are some of the most prevalent and sought-after sets in contemporary times.
+          </div>
+
+          <AboutCard />
+
+        </div>
+      </div>
     </div>
   )
 }
