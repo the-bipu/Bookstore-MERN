93ab012ac8bd41f6a55be2231c17d52f6828fe08
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-12-15 00:38:49 +0530

    Update Hero.tsx

--- frontend-next/pages/Hero.tsx
+++ frontend-next/pages/Hero.tsx
@@ -4,6 +4,7 @@ import AboutCard from '@/components/AboutCard'
 import TotalDepartment from '@/components/TotalDepartment'
 import CustomSlider from '@/components/SliderHome'
 import AboutSite from '@/components/AboutSite'
+import Parallax from '@/components/Parallax'
 
 function Hero() {
   return (
@@ -29,6 +30,20 @@ function Hero() {
 
       {/* About Site Section ( Still need modification ) */}
       <AboutSite />
+
+      {/* All Departments Section */}
+      <div className='py-8'>
+        <div className='lg:text-[25px] text-[20px] font-semibold mt-4 mb-4 text-black text-center'>
+          Our <span className='text-[#22CFB8]'>Departments 🍃</span>
+        </div>
+        <div className='mb-8 text-black text-center'>
+          Presented herein are some of the most prevalent and sought-after sets in contemporary times.
+        </div>
+
+        <TotalDepartment />
+      </div>
+
+      <Parallax />
 
       {/* All Departments Section */}
       <div className='py-8'>
