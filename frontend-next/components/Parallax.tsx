bd582bf7439faf9bf0d3546a3c89b779eaffbd29
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-12-15 00:38:51 +0530

    Create Parallax.tsx

---
+++ frontend-next/components/Parallax.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Parallax = () => {
+  return (
+    <div className='parallax--main'>
+        <div className="parallax--container">
+            <div className='font-semibold text-4xl'>
+                Education for Tomorrow's Leaders
+            </div>
+            <div className='md:w-[50%] w0-[90%] text-lg'>
+                Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.
+            </div>
+            <div className="py-[.6rem] px-[1.5rem] bg-[#234df6] rounded-full mt-4">
+                Explore More
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Parallax
