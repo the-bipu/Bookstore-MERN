54f36934178d5606405ee29f60d93e5ca0cb3e93
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-12-18 07:39:47 +0530

    Create BookCardTotal.tsx

---
+++ frontend-next/components/BookCardTotal.tsx
@@ -0,0 +1,33 @@
+import React, { Key } from "react";
+import BookCard from "./BookCard";
+
+type BookItem = {
+    _id: Key | null | undefined;
+    publishyear: string;
+    title: string;
+    author: string;
+};
+
+interface Book extends BookItem {
+    _id: string | null | undefined;
+}
+
+interface BookCardTotalProps {
+    books: Book[];
+}
+
+const BookCardTotal: React.FC<BookCardTotalProps> = ({ books }) => {
+    return (
+        <div>
+            <div className='flex flex-col w-full h-auto'>
+                <div className='md:px-[5rem] px-[2rem] pt-0 pb-12 flex flex-wrap flex-row items-center justify-center gap-6'>
+                    {books.map((item) => (
+                        <BookCard key={item._id} book={item} />
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default BookCardTotal;
