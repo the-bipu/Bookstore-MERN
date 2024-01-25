8429d9eb168ec896f7198593f5e649a84b0f7076
the-bipu <127449591+the-bipu@users.noreply.github.com>
2024-01-25 23:56:29 +0530

    minor fix

--- frontend-next/pages/About.tsx
+++ frontend-next/pages/About.tsx
@@ -6,7 +6,7 @@ function About() {
     <div className='gradientLinear'>
       <div className='heading-link'>
         <div className='py-6'>
-          <h3>About Us</h3>
+          <h3 className='text-white'>About Us</h3>
           <p> <a href="/">home</a> / about </p>
         </div>
       </div>
