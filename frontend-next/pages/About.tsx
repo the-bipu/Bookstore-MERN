54accae93705dded470293cab01376eae1519b57
the-bipu <127449591+the-bipu@users.noreply.github.com>
2024-01-26 23:09:13 +0530

    update about page

--- frontend-next/pages/About.tsx
+++ frontend-next/pages/About.tsx
@@ -5,14 +5,19 @@ function About() {
   return (
     <div className='gradientLinear'>
       <div className='heading-link'>
-        <div className='py-6'>
-          <h3 className='text-white'>About Us</h3>
-          <p> <a href="/">home</a> / about </p>
+        <div className='flex flex-col gap-4 items-center justify-center pt-44 pb-28'>
+          <div className='lg:w-1/2 w-11/12 text-3xl font-medium mb-2 text-white lufga'>Welcome to Our Literary World</div>
+          <div className='lg:w-8/12 w-11/12 lg:text-8xl text-4xl font-semibold leading-loose mb-6 lufga text-white capitalize'>Discover our Stories & Vision Here.</div>
+          <div className='lg:w-1/2 w-11/12 text-xl font-normal mb-10 text-white lufga'>Embark on a Journey Through Our Literary Universe: Where Stories Spark Imagination and Ignite Passion</div>
+          <div className='flex flex-row gap-8'>
+          <button className='px-6 py-3 text-base font-semibold rounded-md customWhite customShadow'>Explore</button>
+          <button className='px-6 py-3 text-base font-semibold rounded-md customBlue customShadow'>Our Team</button>
+        </div>
         </div>
       </div>
-      <div className='flex flex-col w-full items-center h-auto'>
-        <div className='flex flex-col text-center lg:w-full w-[90%] pt-[20px] pb-[40px] lg:px-[10%] px-4'>
-          <div className='lg:text-[25px] text-[20px] font-semibold mt-4 mb-4 text-black text-center'>
+      {/* <div className='flex flex-col w-full items-center h-auto bg-white[#5956E9]'>
+        <div className='flex flex-col text-center lg:w-full w-11/12 pt-[20px] pb-[40px] lg:px-[10%] px-4'>
+          <div className='lg:text-[25px] text-[20px] font-semibold mt-4 mb-4 text-white text-center lufga'>
             Why <span className='text-[#22CFB8]'>EBooksMania🚀</span>
           </div>
           <div className='mb-8 text-black'>
@@ -22,7 +27,7 @@ function About() {
           <AboutCard />
 
         </div>
-      </div>
+      </div> */}
     </div>
   )
 }
