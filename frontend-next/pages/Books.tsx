2eb0ad1bfadc9af8c41a42011d90014a05cf9e94
the-bipu <127449591+the-bipu@users.noreply.github.com>
2024-01-27 17:31:07 +0530

    update store page

--- frontend-next/pages/Books.tsx
+++ frontend-next/pages/Books.tsx
@@ -2,6 +2,7 @@ import BookCard from '@/common/components/BookCard/BookCard';
 import BookCardTotal from '@/components/BookCardTotal';
 import TotalDepartment from '@/components/TotalDepartment'
 import axios from 'axios';
+import Image from 'next/image';
 import React, { useEffect, useState } from 'react'
 
 function Books() {
@@ -26,20 +27,28 @@ function Books() {
   return (
     <div className='gradientLinear'>
       <div className='heading-link'>
-        <div className='py-6'>
-          <h3>Our Books</h3>
-          <p> <a href="/">home</a> / books </p>
+        <div className=' w-8/12 flex flex-col gap-4 items-center justify-center pt-44 pb-10'>
+          <div className='lg:w-full w-11/12 lg:text-8xl text-4xl font-semibold leading-loose mb-6 lufga text-white '>Explore Our Diverse Book Collection</div>
+          <div className='lg:w-full w-11/12 text-xl font-normal mb-10 text-white lufga'>Discover a Wealth of Titles Across Various Genres and Departments in Our Library.</div>
+          <div className='flex flex-row gap-8'>
+            <button className='px-6 py-3 text-base font-semibold rounded-md customWhite customShadow'>Explore</button>
+            <button className='px-6 py-3 text-base font-semibold rounded-md customBlue customShadow'>Our Team</button>
+          </div>
+          <div className='lg:w-8/12 w-11/12 h-80 relative flex items-center justify-center'>
+            <Image src={'/store/books-city.png'} alt='' width={900} height={600} className=' absolute w-full -bottom-32' />
+          </div>
         </div>
       </div>
-      <div className='py-8'>
-        <div className='lg:text-[25px] text-[20px] font-semibold mt-4 mb-4 text-black text-center'>
+
+      <div className='py-8 bg-white pt-32 pb-28'>
+        {/* <div className='lg:text-[25px] text-[20px] font-semibold mt-4 mb-4 text-black text-center'>
           Our <span className='text-[#22CFB8]'>Book Store 🍃</span>
         </div>
         <div className='mb-8 text-black text-center'>
           Presented herein are some of the most prevalent and sought-after sets in contemporary times.
         </div>
 
-        <BookCardTotal books={books} />
+        <BookCardTotal books={books} /> */}
       </div>
     </div>
   )
