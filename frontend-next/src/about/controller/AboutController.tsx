2b9cc394d9fd2190695284dcac5f5de3df9ca7eb
The-Bipu <127449591+the-bipu@users.noreply.github.com>
2024-03-26 20:56:43 +0530

    create about page

---
+++ frontend-next/src/about/controller/AboutController.tsx
@@ -0,0 +1,35 @@
+import Footer from '@/components/Footer';
+import Navbar from '@/components/Navbar';
+import React, { useState } from 'react';
+
+const AboutController = (props?: any) => {
+    const {
+        children,
+        ...rest
+    } = props;
+
+    const [loading, setLoading] = useState<boolean>(false);
+
+    const viewProps = {
+        loading,
+        setLoading,
+    };
+
+    const childrenWithProps = React.Children.map(children, (child) => {
+        if (React.isValidElement(child)) {
+            return React.cloneElement(child, { ...rest, ...viewProps });
+        }
+
+        return child;
+    });
+
+    return (
+        <div>
+            <Navbar />
+            {childrenWithProps}
+            <Footer />
+        </div>
+    );
+};
+
+export { AboutController };
