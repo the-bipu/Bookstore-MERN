b7e426ba0a6f26faab9f0daa6ca2d6494231e65e
the-bipu <127449591+the-bipu@users.noreply.github.com>
2024-03-28 21:20:01 +0530

    update meta

--- frontend-next/pages/books.tsx
+++ frontend-next/pages/books.tsx
@@ -9,21 +9,21 @@ const BooksPage: NextPage = (props) => (
   <div>
     <BooksController {...props}>
       <Head>
-        <link rel="icon" href="/books-main.png" type="image/png" sizes="70x70" />
-        <title></title>
-        <meta name="description" content="" />
+        <link rel="icon" href="/books.svg" type="image/svg" sizes="70x70" />
+        <title>The Storage Yard - BookStorm | 2024</title>
+        <meta name="description" content="Welcome to our online bookstore where you can find a wide selection of eBooks for all genres. Browse, download, and enjoy reading anytime, anywhere." />
 
-        <meta name="keywords" content="" />
+        <meta name="keywords" content="online bookstore, eBooks, buy books online, download books, digital books, reading, literature, fiction, non-fiction" />
 
-        <meta name="author" content="Internpluss.com" />
-        <meta property="og:title" content="" />
-        <meta property="og:description" content="" />
-        <meta property="og:image" content="/og/books-og.png" />
-        <meta property="og:url" content="" />
+        <meta name="author" content="https://bookstorm.vercel.app/" />
+        <meta property="og:title" content="Online Bookstore - BookStorm | 2024" />
+        <meta property="og:description" content="Welcome to our online bookstore where you can find a wide selection of eBooks for all genres. Browse, download, and enjoy reading anytime, anywhere." />
+        <meta property="og:image" content="/og/bookstore.svg" />
+        <meta property="og:url" content="https://bookstorm.vercel.app/" />
 
-        <meta name="twitter:card" content="/og/twitter-og.png" />
-        <meta name="twitter:title" content="" />
-        <meta name="twitter:description" content="" />
+        <meta name="twitter:card" content="/og/bookstore.svg" />
+        <meta name="twitter:title" content="Online Bookstore - BookStorm | 2024" />
+        <meta name="twitter:description" content="Welcome to our online bookstore where you can find a wide selection of eBooks for all genres. Browse, download, and enjoy reading anytime, anywhere." />
         <meta name="twitter:image" content="/og/twitter-og.png" />
       </Head>
 
